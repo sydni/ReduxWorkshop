4acaad405da378e49b9dec897c526e9fd2029c57
Sydni Topper <sydni.topper.18@dartmouth.edu>
2016-07-26 15:53:02 -0400

    workshop finished

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Counter from '../containers/counter';
+import Controls from '../containers/controls';
 
 
 // function based "dumb" component with no state
 const Welcome = () => {
   return (
     <div>
-      react+react-router+webpack+babel+eslint starter
+      <Counter />
+      <Controls />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ import './style.scss';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
+// at the top
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
 
-// entry point that just renders app
-// could be used for routing at some point
+
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// replace your ReactDOM render with the following
+// note this uses the Router stuff from last week
 ReactDOM.render(
-  <Router history={browserHistory} routes={routes} />
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
   , document.getElementById('main'));
